15a7690b2495f3b7f130ecaf8fb3a88663a2c2b1
erkutahmet <90329328+erkutahmet@users.noreply.github.com>
2023-10-12 23:55:23 +0300

    swift Life Cycle

--- Working Structure/Working Structure/HomeScreenVC.swift
+++ Working Structure/Working Structure/HomeScreenVC.swift
@@ -11,10 +11,28 @@ class HomeScreenVC: UIViewController {
 
     @IBOutlet weak var homeScreenLabel: UILabel!
 
-    override func viewDidLoad() {
+    override func viewDidLoad() { // The application runs the first time it is opened (it only runs once)
         super.viewDidLoad()
         homeScreenLabel.text = "Welcome\nHome Screen"
+        print("viewDidLoad() runned")
     }
+    
+    override func viewWillAppear(_ animated: Bool) { // Runs whenever the page is appear
+        print("viewWillAppear() runned")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) { // Runs whenever the page is disappear
+        print("viewWillDisappear() runned")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) { // Runs after the page is appear
+        print("viewDidAppear runned")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) { // Runs after page becomes disappear
+        print("viewDidDisappear runned")
+    }
+    
     @IBAction func makeButtonClicked(_ sender: Any) {
         homeScreenLabel.text = "Hello"
     }
@@ -25,7 +43,7 @@ class HomeScreenVC: UIViewController {
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        print("prepare func worked")
+        print("prepare func runned")
         if segue.identifier == "toGameScreen" {
             print("toGameScreen passed")
             
